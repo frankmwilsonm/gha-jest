a52e28fa526f903b2efe4e118370c6028f6b708a
frankmwilsonm <frank.mamani@upeu.edu.pe>
2023-07-06 01:58:01 -0500

    new add

--- src/sapp.js
+++ src/sapp.js
@@ -6,6 +6,54 @@ import express from "express";
 import express from "express";
 
 
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  });
 
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  }); 
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  });
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  }); 
+
+  
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  });
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  }); 
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  });
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  }); 
+
+  
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  });
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  }); 
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  });
+
+app.post("/newuser", (req, res) => {
+    res.status(200).json();
+  }); 
 
 export default app;

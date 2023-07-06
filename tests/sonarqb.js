5f4a02eb7ea5bd17d38d66a35a709666f4d4df58
Frank Wilson <54363062+frankmwilsonm@users.noreply.github.com>
2023-07-06 02:26:03 -0500

    add siq

---
+++ tests/sonarqb.js
@@ -0,0 +1,8 @@
+const exec = require('child_process').exec;
+
+function executeCommand(userInput) {
+  exec('echo ' + userInput);
+}
+
+const userInput = process.argv[2];
+executeCommand(userInput);
